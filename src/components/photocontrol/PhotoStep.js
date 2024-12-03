7074b85e5565fdca2f0b6cab9a5357a5554e711f
abdullox0900 <abdullox0900@gmail.com>
2024-12-03 23:07:41 +0500

    change style photostep.js file 27

--- src/components/photocontrol/PhotoStep.js
+++ src/components/photocontrol/PhotoStep.js
@@ -29,14 +29,7 @@ export default function PhotoStep({ initCamera, camera, code, title, keyboard, r
       </div>
 
       <div style={{ textAlign: 'center' }} >
-        {
-          stepPhoto !== null && (
-            <button className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''
-              } w-full px-[20px] py-[12px] rounded-[20px] font-bold mb-[20px] text-[24px] text-[#181C1E] bg-transparent border-[2px] border-solid border-[#181C1E]`} id={"next"} block color='primary' onClick={() => {
-                onNext(stepPhoto)
-              }}>Далее</button>
-          )
-        }
+
         {
           keyboard.map((item) => (
             <button className='flex items-center justify-center mx-auto gap-[20px] w-full px-[20px] py-[12px] rounded-[20px] bg-[#FFD12E] font-proxima text-[24px] text-[#181C1E]' id={item.code} block color='primary' onClick={() => {
@@ -49,6 +42,14 @@ export default function PhotoStep({ initCamera, camera, code, title, keyboard, r
             !repeatMode && (
               <button className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''
                 } w-full px-[20px] py-[12px] rounded-[20px] font-bold text-[24px] text-[#181C1E] bg-transparent border-[2px] border-solid border-[#181C1E]`} id={"prev"} block color='primary' onClick={() => { onPrev() }}>Назад</button>
+            )
+          }
+          {
+            stepPhoto !== null && (
+              <button className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''
+                } w-full px-[20px] py-[12px] rounded-[20px] font-bold mb-[20px] text-[24px] text-[#181C1E] bg-transparent border-[2px] border-solid border-[#181C1E]`} id={"next"} block color='primary' onClick={() => {
+                  onNext(stepPhoto)
+                }}>Далее</button>
             )
           }
           {/* <button className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''
