ba75acc1b8deab9e473931a3c0948284e69cf69d
abdullox0900 <abdullox0900@gmail.com>
2024-12-04 16:12:53 +0500

    change style photostep.js file 33

--- src/components/photocontrol/RepeatStep.js
+++ src/components/photocontrol/RepeatStep.js
@@ -1,28 +1,48 @@
-import React  from "react";
-import {Button, Space} from "antd-mobile";
+import React from "react"
+import { useDarkMode } from '../../contexts/DarkModeContext'
 
-export default function RepeatStep(props) {
+export default function RepeatStep({ workflow, onStepSelected, onOK, onCancel }) {
+  const isDarkMode = useDarkMode()
+
   return (
-    <>
-      <h3 style={{textAlign: 'center'}}>
-        Выберите этап контроля для изменения
-      </h3>
+    <div className='container'>
+      <h2 className={`${isDarkMode ? 'text-[#fff]' : ''} text-[24px] mb-[20px] font-proxima font-bold text-center`}>
+        Выберите этап для перефотографирования
+      </h2>
 
-      <div style={{textAlign: 'center'}}>
-        <Space direction={"vertical"} style={{width: '90%'}}>
-          {
-            props.workflow
-              .filter(item => item.menu_item)
-              .map((item, index) => (
-                <Button key={item.code} block color='default' onClick={() => {
-                  props.onStepSelected(item.code)
-                }}>{item.menu_item}</Button>
-              ))
-          }
-          <Button key={"ok"} block color='primary' onClick={() => { props.onOK() }}>Завершить</Button>
-          <Button key={"cancel"} block color='primary' onClick={() => { props.onCancel() }}>Отменить контроль</Button>
-        </Space>
+      <div className='flex flex-col gap-4'>
+        {workflow.map((item) => (
+          <button
+            key={item.code}
+            className='flex items-center justify-center mx-auto gap-[20px] w-full px-[20px] py-[12px] rounded-[20px] bg-[#FFD12E] font-proxima text-[24px] text-[#181C1E]'
+            onClick={() => {
+              onStepSelected(item.code)
+            }}
+          >
+            {item.menu_item}
+          </button>
+        ))}
+
+        <div className='flex items-center justify-center gap-[20px] w-full'>
+          <button
+            className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''} w-full px-[20px] py-[12px] rounded-[20px] font-bold text-[24px] text-[#181C1E] bg-transparent border-[2px] border-solid border-[#181C1E]`}
+            onClick={() => {
+              onCancel()
+            }}
+          >
+            Отмена
+          </button>
+
+          <button
+            className={`${isDarkMode ? 'text-[#fff] border-[#fff]' : ''} w-full px-[20px] py-[12px] rounded-[20px] font-bold text-[24px] text-[#181C1E] bg-transparent border-[2px] border-solid border-[#181C1E]`}
+            onClick={() => {
+              onOK()
+            }}
+          >
+            Готово
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
